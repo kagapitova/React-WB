214c491840e14462081ad9d86fc0ebd278ec6098
kseniaagapitova <kagapitova@inbox.ru>
2024-12-15 00:26:29 +0200

    shop: init

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
-import { Inventory } from "./Inventory";
-
 const App = () => (
   <div className="App">
-     <Inventory jsonFile="inventory1.json" />
-     <Inventory jsonFile="inventory2.json" />
   </div>
 );
 
